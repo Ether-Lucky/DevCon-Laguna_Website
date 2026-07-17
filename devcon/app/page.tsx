6ca58edcaf72bd2d517bfd40f2ca56dfc4eb1069
judepadillx <judepadillx@gmail.com>
2026-07-17 09:39:39 +0800

    chore: partial layout of navbar

--- devcon/app/page.tsx
+++ devcon/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import NavLinks from "@/components/ui/nav-links";
 
 export default function Home() {
   return (
     <>
-      <nav></nav>
+      <nav className='flex flex-row gap-16'>
+        {/* Logo */}
+        <NavLinks />
+        <button>Join Us</button>
+        {/* Theme Button */}
+      </nav>
       <main></main>
       <footer></footer>
     </>
