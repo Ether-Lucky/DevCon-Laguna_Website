88bfb74bec9067d740fc59aae84fa6f29fb2a1c8
justinbril abarquiz <justinbrilabarquez@gmail.com>
2026-07-19 22:13:31 +0800

    feat: added the events section

--- devcon/app/page.tsx
+++ devcon/app/page.tsx
@@ -1,17 +1,13 @@
-import Button from '@/components/ui/button';
+import React from 'react';
 import Carousel from '@/components/ui/events-carousel';
-import WhatWeDo from '@/components/ui/what-we-do';
+
 
 export default function Home() {
   return (
-    <>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-devcon-black p-24 text-devcon-white font-inter"> 
 
-      <main className="flex min-h-screen flex-col items-center justify-center bg-devcon-black p-24 text-devcon-white font-inter">  
-        
-        <WhatWeDo />
+      <Carousel />
 
-      </main>
-      <footer></footer>
-    </>
-  )
+    </main>
+  );
 }
