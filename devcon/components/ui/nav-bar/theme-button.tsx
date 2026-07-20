a87ccbc4b25c36cdfd12a100b0ab4d0224de1dc3
judepadillx <judepadillx@gmail.com>
2026-07-20 15:05:43 +0800

    test: add files i cannot pull in prod

--- devcon/components/ui/nav-bar/theme-button.tsx
+++ devcon/components/ui/nav-bar/theme-button.tsx
@@ -22,3 +22,5 @@ export default function ThemeButton({ theme } : { theme: 'light' | 'dark' }) {
     </button>
   );
 }
+
+dsaf
--- devcon/components/ui/what-we-do.tsx
+++ devcon/components/ui/what-we-do.tsx
@@ -110,3 +110,5 @@ export default function WhatWeDo() {
     </section>
   );
 }
+
+asff
