33e1f561d7fe58ebaedd3c96d42143f4e7204b32
judepadillx <judepadillx@gmail.com>
2026-07-20 14:45:42 +0800

    feat: hero and stats sections

--- devcon/app/page.tsx
+++ devcon/app/page.tsx
@@ -1,11 +1,11 @@
-import Hero from "@/components/ui/sections/hero";
+import Hero from '@/components/ui/sections/hero';
+import Stats from '@/components/ui/sections/stats';
 
 export default function Home() {
   return (
-    <>
-      <main>
-        <Hero></Hero>
-      </main>
-    </>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-devcon-black p-24 text-devcon-white font-inter">  
+      <Hero />
+      <Stats />
+    </main>
   );
 }
