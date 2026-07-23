92fbfa19b0e75ab97c1b1b7740670bd4a407d6e6
judepadillx <judepadillx@gmail.com>
2026-07-23 17:13:47 +0800

    chore: logo change theme

--- devcon/components/ui/logo.tsx
+++ devcon/components/ui/logo.tsx
@@ -3,13 +3,23 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <div className="shrink-0 leading-none">
+      {/* Shown in dark theme */}
       <Image
-        src="/logo/devcon-logo.png"
+        src="/logo/dark-logo.png"
         alt="Devcon Logo"
         width={240}
         height={76}
-        className="h-auto w-[170px] md:w-[200px]"
         priority
+        className="hidden h-auto w-[170px] dark:block md:w-[200px]"
+      />
+      {/* Shown in light theme */}
+      <Image
+        src="/logo/light-logo.png"
+        alt="Devcon Logo"
+        width={240}
+        height={76}
+        priority
+        className="block h-auto w-[170px] dark:hidden md:w-[200px]"
       />
       <span className="block text-right text-[24px] font-bold uppercase tracking-[0.01em] text-muted md:text-[24px]">
         Laguna
