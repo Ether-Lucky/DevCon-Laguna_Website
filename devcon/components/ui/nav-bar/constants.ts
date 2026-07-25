0904f57ff117025cdae29b4b89c05bb812492124
judepadillx <judepadillx@gmail.com>
2026-07-25 22:21:48 +0800

    chore: remove links from nav bar

--- devcon/components/ui/nav-bar/constants.ts
+++ devcon/components/ui/nav-bar/constants.ts
@@ -1,10 +1,10 @@
 export const NAV_LINKS = [
   { name: "Home", href: "/" },
-  { name: "About", href: "/#about" },
-  { name: "Events", href: "/#events" },
-  { name: "Officers", href: "/#officers" },
-  { name: "Partners", href: "/#partners" },
-  { name: "Contact", href: "/#contact" },
+  { name: "About", href: "/" },
+  { name: "Events", href: "/" },
+  { name: "Officers", href: "/" },
+  { name: "Partners", href: "/" },
+  { name: "Contact", href: "/" },
 ] as const;
 
 /** Desktop nav appears above this width; mobile menu below. */
